b998a53c7cf94d9d30315e9aa5b639b8ccee84d5
adan_daniel <adan@joinheard.com>
2025-09-22 11:39:07 -0400

    test

--- ecr.tf
+++ ecr.tf
@@ -2,3 +2,6 @@ resource "aws_ecrpublic_repository" "arroyo-ecr" {
   repository_name = "${local.proyect_name}-ecr"
 
 }
+
+output "ecr_repository_name" { value = aws_ecrpublic_repository.arroyo-ecr.repository_name }
+output "ecr_repository_url"  { value = aws_ecrpublic_repository.arroyo-ecr.repository_uri }
